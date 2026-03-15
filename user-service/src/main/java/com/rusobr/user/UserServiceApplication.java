b9dea58823913c6bd4434c3fad2884ebf484679c
Alexey Kochetygov <82048733+M8eight@users.noreply.github.com>
2026-03-15 11:50:30 +0300

    feat: Добавил таблицы parent, teacher, student и связал с users. Closes #19 (#20)

--- user-service/src/main/java/com/rusobr/user/UserServiceApplication.java
+++ user-service/src/main/java/com/rusobr/user/UserServiceApplication.java
@@ -3,11 +3,13 @@ package com.rusobr.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableWebMvc
+@EnableJpaAuditing
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
