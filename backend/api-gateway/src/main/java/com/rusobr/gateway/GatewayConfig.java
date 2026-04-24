0ac581825dbf35942ec55a78d061cb9533eb8d07
M8eight <alekskoch234@gmail.com>
2026-04-24 21:10:42 +0300

    fix: перенес cors проверку в gateway, из сервисов,

--- backend/api-gateway/src/main/java/com/rusobr/gateway/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/rusobr/gateway/GatewayConfig.java
@@ -3,7 +3,12 @@ package com.rusobr.gateway;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Configuration
 public class GatewayConfig {
@@ -19,5 +24,19 @@ public class GatewayConfig {
                 System.out.println("<<< Response: " + exchange.getResponse().getStatusCode());
             }));
         };
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
